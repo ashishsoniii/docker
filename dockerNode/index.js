313f2e8e9f2ca8ce944113e90f4b930140664b50
Ashish Soni <ashishsoni2002@gmail.com>
2024-12-09 01:26:51 +0530

    Docker project test

---
+++ dockerNode/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const port = 8000;
+
+// Simple GET 
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello ashish from docker!" });
+});
+// Simple GET request with query parameter
+app.get("/getWithQuery", (req, res) => {
+  const name = req.query.name;
+  res.json({ msg: `Hello, ${name}` });
+});
+
+// Another simple GET request
+app.get("/anotherGet", (req, res) => {
+  res.json({ msg: "This is another GET request." });
+});
+
+// Simple POST request with JSON body
+app.post("/postWithName", (req, res) => {
+  const name = req.body.name;
+  res.json({ msg: `Hello, ${name}` });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
